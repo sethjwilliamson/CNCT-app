d38d2108f83a551556d65e0e527134fe9f9e6341
Rachel Ferrara <rferra4@lsu.edu>
2019-02-14 17:38:19 -0600

    int
    test

--- JavaApplication167/src/javaapplication167/JavaApplication167.java
+++ JavaApplication167/src/javaapplication167/JavaApplication167.java
@@ -15,7 +15,6 @@ public class JavaApplication167 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        int n =2;    }
     
 }

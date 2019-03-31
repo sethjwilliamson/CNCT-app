82176f9ad93e74b70d472222a52fbe55b03b1e58
Seth Jackson <sethjack2356@gmail.com>
2019-03-31 18:18:47 -0500

    Create Youtube.py

---
+++ WebApplication1/Scripts/Youtube/Youtube.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import google.oauth2.credentials
+
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret.
+# Make this equal to the directory of the client_secret.json file
+CLIENT_SECRETS_FILE = "client_secret.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+def get_authenticated_service():
+  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+  credentials = flow.run_console()
+  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+
+def subscriptions_list_my_subscriptions(client, **kwargs):
+  response = client.subscriptions().list(
+    **kwargs
+  ).execute()
+  print(response)
+	
+if __name__ == '__main__':
+  # When running locally, disable OAuthlib's HTTPs verification. When
+  # running in production *do not* leave this option enabled.
+  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+  client = get_authenticated_service()
+  
+  subscriptions_list_my_subscriptions(client,
+    part='snippet,contentDetails',
+    mine=True)

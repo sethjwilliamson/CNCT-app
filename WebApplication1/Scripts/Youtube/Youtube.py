31509405b5e05969ed0305260be5362e9387b055
Seth Jackson <sethjack2356@gmail.com>
2019-04-01 22:18:38 -0500

    YAGNI Youtube.py, prep for db upload

--- WebApplication1/Scripts/Youtube/Youtube.py
+++ WebApplication1/Scripts/Youtube/Youtube.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-
 import os
-
+import json
+import pymysql
 import google.oauth2.credentials
-
 import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -12,32 +10,26 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 # The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
 # the OAuth 2.0 information for this application, including its client_id and
 # client_secret.
-# Make this equal to the directory of the client_secret.json file
-CLIENT_SECRETS_FILE = "client_secret.json"
+clientSecrets = "./client_secret.json"
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection.
-SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
-API_SERVICE_NAME = 'youtube'
-API_VERSION = 'v3'
+scopes = ['https://www.googleapis.com/auth/youtube.force-ssl']
+apiServiceName = 'youtube'
+apiVersion = 'v3'
 
-def get_authenticated_service():
-  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-  credentials = flow.run_console()
-  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+def getAccessToken():
+  flow = InstalledAppFlow.from_client_secrets_file(clientSecrets, scopes)
+  credentials = flow.run_local_server()
+  return build(apiServiceName, apiVersion, credentials = credentials)
 
-def subscriptions_list_my_subscriptions(client, **kwargs):
-  response = client.subscriptions().list(
-    **kwargs
-  ).execute()
-  print(response)
+# the parameter **kwargs allows for multiple keyword arguments
+def listSubcriptions(client, **kwargs):
+	data = json.dumps(client.subscriptions().list(**kwargs).execute())
 	
-if __name__ == '__main__':
-  # When running locally, disable OAuthlib's HTTPs verification. When
-  # running in production *do not* leave this option enabled.
-  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-  client = get_authenticated_service()
-  
-  subscriptions_list_my_subscriptions(client,
-    part='snippet',
-    mine=True)
+def getDatabase():
+	return pymysql.connect(user='cnctsoci_admin', password='Csc3380!!!', host='155.138.243.181', database='cnctsoci_data')
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+client = getAuthentication()
+listSubcriptions(client, part='snippet', mine=True)

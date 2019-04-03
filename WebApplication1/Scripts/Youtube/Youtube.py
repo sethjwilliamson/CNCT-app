946ec5376a6f0f144fde6e0e900b442d3c78c916
Seth Jackson <sethjack2356@gmail.com>
2019-04-02 23:33:37 -0500

    Add database functionality to Youtube.py

--- WebApplication1/Scripts/Youtube/Youtube.py
+++ WebApplication1/Scripts/Youtube/Youtube.py
@@ -4,6 +4,7 @@ import pymysql
 import urllib.request
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
+from datetime import datetime 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -46,7 +47,10 @@ def listSusbcriptions(client, **kwargs):
 		recentVideos = (json.load(urllib.request.urlopen('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=' + playlistId + '&maxResults=50&key=InsertAPIKeyHere')))
 		
 		# Retrieves the videoId from the recentVideos Playlist
-		videoId = recentVideos['items'[i]['snippet']['resourceId']['videoId']]
+		videoId = recentVideos['items'][i]['snippet']['resourceId']['videoId']
+		
+		# Retrieves the videoId from the recentVideos Playlist
+		publishedDate = recentVideos['items'][i]['snippet']['publishedAt']
 		
 		# Retrieves the url for the thumbnail
 		# default.jpg is 120x90
@@ -55,7 +59,18 @@ def listSusbcriptions(client, **kwargs):
 		# sddefault.jpg is 640x480
 		# maxresdefault.jpg is 1280x720
 		videoThumbnail = "https://i.ytimg.com/vi/" + videoId + "/sddefault.jpg"
+		
+def insertSubscriptionVideos(chanId, vid, date, vidThumb,db):
+	cursor = db.cursor()
+	cursor.execute("""INSERT INTO `posts` (`id`, `userid`, `link`, `time`) VALUES (%s, %s, %s, %s) ON DUPLICATE KEY UPDATE `userid` = %s, `link` = %s, `time` = %s;""", ('y' + vid, 'y' + i['user']['id'], ('https://www.youtube.com/watch?v=' + vid), date), 'y' + i['user']['id'], ('https://www.youtube.com/watch?v=' + vid), date)
+	cursor.execute("""INSERT INTO `media` (`id`,`postid`, `link`) VALUES (NULL, %s, %s) ON DUPLICATE KEY UPDATE `postid` = %s, `link` = %s;""", (i['images']['standard_resolution']['url'], ('y' + vid) , vidThumb))
+	print("cool stuff")
+	cursor.close()
 
+def getDatabase():
+	return pymysql.connect(user='cnctsoci_admin', password='Csc3380!!!', host='155.138.243.181', database='cnctsoci_data')
+
+db = getDatabase()
 # This can be disabled for local use, but don't disable it for commercial use
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
@@ -64,3 +79,8 @@ client = getAccessToken()
 
 #Get a list of subscriptions, I'm going to keep maxResults low for testing purposes
 listSusbcriptions(client, part='snippet', mine=True, maxResults=5)
+
+insertSubscriptionVideos('UClFSU9_bUb4Rc6OYfTt5SPw', '4ChP2cLdmvw', '2019-04-02 21:00:25', 'https://i.ytimg.com/vi/4ChP2cLdmvw/maxresdefault.jpg', db)
+
+db.commit()
+db.close()
